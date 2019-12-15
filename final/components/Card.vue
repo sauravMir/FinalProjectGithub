529d6937e2837d520bbf169dc577ac66c052626b
sauravMir <saurav_41@yahoo.com>
2019-12-16 00:31:42 +0900

    Update Card.vue
    yes or no added

--- final/components/Card.vue
+++ final/components/Card.vue
@@ -8,20 +8,18 @@
       </header>
       <div class="card-content">
         <div class="content has-text-centered">
-          <b-icon
-            :icon="icon"
-            size="is-large"
-            type="is-primary"
-          />
+          <b-icon :icon="icon" size="is-large" type="is-primary" />
         </div>
       </div>
-      <footer class="card-footer">
-        <div class="card-footer-item">
-          <span>
-            <slot />
-          </span>
+
+      <div class="tile-container ">
+        <div class="tile">
+          <button class="block" style="background-color: orange;">Cancel</button>
         </div>
-      </footer>
+        <div class="tile">
+          <button class="block" style="background-color: green;">Add</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,5 +36,37 @@ export default {
       required: true
     }
   }
-}
+};
 </script>
+
+<style scoped>
+.tile-container {
+  position: relative;
+  width: 100%;
+  font-size: 0; /* add */
+}
+
+.tile {
+  font-size: 20px; /* or whatever you want */
+  width: 50%;
+  min-height: 40px;
+  display: inline-block;
+  color: white;
+  
+}
+.center-button {
+  width: 50%px; 
+  margin: 0 auto;
+}
+.block {
+  display: block;
+  width: 100%;
+  height: 50px;
+  border: 1px grey;
+  border-style: solid;
+  text-align: center;
+  margin: auto;
+  color: white;
+  font-weight: bold;
+}
+</style>
